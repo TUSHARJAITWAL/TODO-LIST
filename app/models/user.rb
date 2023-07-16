4fc72636bd2d86051947889a45c20fa84751e5fc
TUSHARJAITWAL <tusharjaitwal015@gmail.com>
2023-07-16 23:10:03 +0530

    ROR-update-TODO

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     validates :name,:email ,presence: true
     validates :email , uniqueness: true
     validates :phone , uniqueness: true
-    validates :name,format: {with: /\A[a-zA-Z]+\z/,message: 'only letters are allowed'}
+    validates :name,format: {with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ ,message: 'only letters are allowed'}
 end
