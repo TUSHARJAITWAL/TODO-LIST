0ed37e917411a2e779b77ebe01c1330ee29fda78
TUSHARJAITWAL <tusharjaitwal015@gmail.com>
2023-07-16 22:26:01 +0530

    TODO-LIST-ROR

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+    def index
+        if params[:search].present?
+            @users = User.where(name: params["search"].capitalize)
+        else
+            @users = User.all
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(users_params)
+        if @user.save
+            redirect_to users_path
+        else
+            render :new , status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(users_params)
+            redirect_to users_path
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path
+    end
+
+    private
+    def users_params
+        params.require(:user).permit(:name,:email,:phone,:address)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    validates :name,:email ,presence: true
+    validates :email , uniqueness: true
+    validates :phone , uniqueness: true
+    validates :name,format: {with: /\A[a-zA-Z]+\z/,message: 'only letters are allowed'}
+end
